137611b3d3c7776741e62413f7c2d987add1d3db
rhhen122 <rokyhenderson222@gmail.com>
2025-05-30 10:14:58 +1200

    modified:   docs/index.html modified:   docs/main.js modified:   docs/main.ts

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,13 @@
 var logo = "./logo.svg";
 var name = "Angstrom";
+function updateTime() {
+    var timeDiv = document.querySelector('.time');
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+    timeDiv.textContent = "".concat(hours, ":").concat(minutes, ":").concat(seconds);
+}
 var link = document.querySelector("link[rel~='icon']");
 if (!link) {
     link = document.createElement('link');
@@ -10,3 +18,5 @@ link.href = logo;
 document.getElementById("title").innerText = name;
 document.getElementById("logo").src = logo;
 document.getElementById("name").innerText = name;
+updateTime();
+setInterval(updateTime, 1000);
--- docs/main.ts
+++ docs/main.ts
@@ -2,6 +2,14 @@ var logo: string = "./logo.svg";
 var name: string = "Angstrom";
 
 
+function updateTime() {
+    const timeDiv = document.querySelector('.time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    timeDiv.textContent = `${hours}:${minutes}:${seconds}`;
+}
 
 var link = document.querySelector("link[rel~='icon']");
 if (!link) {
@@ -13,3 +21,6 @@ link.href = logo;
 document.getElementById("title").innerText = name;
 document.getElementById("logo").src = logo;
 document.getElementById("name").innerText = name;
+
+updateTime();
+setInterval(updateTime, 1000);
