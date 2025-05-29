db49cec2670409a6e02b5c74d1eaf0d4e4698f46
rhhen122 <rokyhenderson222@gmail.com>
2025-05-30 11:28:01 +1200

    I better have fixed this i swear to god

--- docs/main.ts
+++ docs/main.ts
@@ -3,6 +3,15 @@ var name: string = "Angstrom";
 
 
 
+function updateTime() {
+    var timeDiv = document.querySelector('.time');
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+    timeDiv.textContent = "".concat(hours, ":").concat(minutes, ":").concat(seconds);
+}
+
 var link = document.querySelector("link[rel~='icon']");
 if (!link) {
     link = document.createElement('link');
@@ -13,3 +22,5 @@ link.href = logo;
 document.getElementById("title").innerText = name;
 document.getElementById("logo").src = logo;
 document.getElementById("name").innerText = name;
+updateTime();
+setInterval(updateTime, 1000);
--- docs/start.sh
+++ docs/start.sh
@@ -3,11 +3,12 @@ echo 'Compiling TS'
 sass style.scss style.css > /dev/null 2>&1
 echo 'Compiling SCSS'
 echo 'Running Web Server'
-read -p '1 PHPWS, 2. PYWS :' server
-if [ server == "1" ]
+read -p '1 PYWS, 2. PHPWS :' server
+if [ $server == "1" ];
 then
-  python3 -m http.server 8888
-else
-  php -S localhost:8888
+    python3 -m http.server 8888
+elif [ $server == "2" ];
+then
+    php -S localhost:8888
 fi
 echo 'http://localhost:8888'
